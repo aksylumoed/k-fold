9d4ba098e5ba9f49d702594935a2668220284543
Oana Miron <oana9opt@yahoo.com>
2019-04-21 01:19:43 +0200

    Update kfold.py

--- kfold.py
+++ kfold.py
@@ -2,9 +2,10 @@ import numpy as np
 from random import seed
 from random import randrange
 import features as feat
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-
-filename="mfeat-pix.txt"
+filename = "mfeat-pix.txt"
 
 data1 = np.loadtxt(filename)
 
@@ -13,19 +14,22 @@ print(data.shape)
 train = data.tolist()
 test = data[1::2]
 
-target = []
+train_y = []
 
 for i in range(10):
-    ones = np.zeros((100,10))
+    ones = np.zeros((100, 10))
     ones[:, i] = 1
-    target.append(ones.tolist())
-target = np.reshape(np.ravel(target), (1000, 10))
+    train_y.append(ones.tolist())
+train_y = np.reshape(np.ravel(train_y), (1000, 10))
+
 
 """
 RANDOMLY splits the dataset and binary set into n-folds
 """
+
+
 def split(dataset, folds=2):
-    t = target.tolist()
+    t = train_y.tolist()
     zset = []
     splitset = []
     data = list(dataset)
@@ -44,87 +48,88 @@ def split(dataset, folds=2):
 
 
 seed(1)
-#print(np.array(kfold(train, 4)).shape)
 
 
-def ridge(phi, target, alpha):
+# print(np.array(kfold(train, 4)).shape)
+
+
+def ridge(phi, train_y, alpha):
     x = np.transpose(phi).dot(phi)
     id = np.identity(phi[0].size)
-    inv = np.linalg.inv(np.add(x, alpha*alpha*id))
+    inv = np.linalg.inv(np.add(x, alpha * alpha * id))
 
-    return inv.dot(np.transpose(phi)).dot(target)
+    return inv.dot(np.transpose(phi)).dot(train_y)
 
 
 """
 Calculates the Mean Squared Error
 """
+
+
 def MSE(d, y):
-    return np.sum((d - y)**2)/len(d)
-
-
-
+    return np.sum((d - y) ** 2) / len(d)
 
 
 def kfold(dataset, binaryset, models, alpha):
     """
-    
+
     :param dataset: this dataset is the set of feature vectors, each with k features
-    :param binaryset: this is the set of binary target vectors z, where all values
-                      are 0 except 1 in index i, for class i 
-    :param models: this is the model parameters we should modify 
+    :param binaryset: this is the set of binary train_y vectors z, where all values
+                      are 0 except 1 in index i, for class i
+    :param models: this is the model parameters we should modify
                    (feature length, types of features)
     :param alpha: alpha for the ridge regression
-    :return: 
+    :return:
     """
-    #risklist = []
+    # risklist = []
     res = []
     for i in range(len(dataset)):
         set = list(dataset)
-        
+
         test_x = np.array(set[i])
         test_y = np.array(binaryset[i])
         train_x = np.array(set)
         train_x = np.delete(train_x, i, 0)
-        train_x = np.reshape(train_x, (750, 101))
+        train_x = np.reshape(train_x, (len(train)-len(test_x), len(set[i][0])))
         print(train_x.shape)
-        target = np.array(binaryset)
-        target = np.delete(np.array(target), i, 0)
-        target = np.reshape(target, (750, 10))
-        print(target.shape)
+        train_y = np.array(binaryset)
+        train_y = np.delete(np.array(train_y), i, 0)
+        train_y = np.reshape(train_y, (len(train)-len(test_x), 10))
+        print(train_y.shape)
 
         rlist = []  # use this to append the MSE values
         for m in range(1):
-
             print("train_x:", train_x)
-            print("Target: ", target)
-            dopt = ridge(train_x, target, alpha)
+            print("train_y: ", train_y)
+            dopt = ridge(train_x, train_y, alpha)
             print("shape: ", dopt.shape)
             trainvotes = np.array(train_x).dot(dopt)
             testvotes = np.array(test_x).dot(dopt)
-            trainresult = MSE(trainvotes, target)
+            trainresult = MSE(trainvotes, train_y)
             testresult = MSE(testvotes, test_y)
 
+            visualizePredictedAndExpected(test_y, testvotes)
 
             rlist.append(trainresult)
             rlist.append(testresult)
 
-            #r = (1/len(dataset)) * np.sum(loss(dopt, target))
-            #rlist.append(r)
+            # r = (1/len(dataset)) * np.sum(loss(dopt, train_y))
+            # rlist.append(r)
 
-        #risk = np.mean(rlist)
+        # risk = np.mean(rlist)
 
         res.append(rlist)
-    #print("Result is: ", res)
+    # print("Result is: ", res)
     sum1 = 0
-    sum2= 0
+    sum2 = 0
     for i in range(len(res)):
         sum1 = sum1 + res[i][0]
         sum2 = sum2 + res[i][1]
 
     result = []
-    result.append(sum1/len(res))
-    result.append(sum2/len(res))
-    #return np.argmin(res) # for now return the result
+    result.append(sum1 / len(res))
+    result.append(sum2 / len(res))
+    # return np.argmin(res) # for now return the result
 
     return result
 
@@ -134,15 +139,15 @@ def main():
     models = []
     result = kfold(dataset, binaryset, models, 3)
     print(result)
+    plt.show()
 
 
-#print(ridge(train, target, 0))
+# print(ridge(train, train_y, 0))
 
 main()
 
-
 """
-x = np.linalg.pinv(train).dot(target)
+x = np.linalg.pinv(train).dot(train_y)
 testvotes = np.array(test).dot(x)
 testresults = [max(p) for p in testvotes]
 testresults = [x - 1 for x in testresults]
@@ -157,4 +162,3 @@ mismatches = np.sum(np.abs(np.sign(np.array(testresults)- np.array(np.ravel(test
 
 error = 100*mismatches/1000
 """
-
