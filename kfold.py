7840042d2610407f08bca7e831c2cc6e2d0c3e1f
aksylumoed <adityadndkr@gmail.com>
2019-04-20 22:12:19 +0200

    Update kfold.py

--- kfold.py
+++ kfold.py
@@ -1,17 +1,29 @@
 import numpy as np
 from random import seed
 from random import randrange
+import features as feat
+
 
 filename="mfeat-pix.txt"
 
-data = np.loadtxt(filename)
+data1 = np.loadtxt(filename)
 
-
-train = data[0::2]
+data = feat.createFeatureVectors()
+print(data.shape)
+train = data.tolist()
 test = data[1::2]
+
+target = []
+
+for i in range(10):
+    ones = np.zeros((100,10))
+    ones[:, i] = 1
+    target.append(ones.tolist())
+target = np.reshape(np.ravel(target), (1000, 10))
 
 
 def split(dataset, folds=2):
+    t = target.tolist()
     zset = []
     splitset = []
     data = list(dataset)
@@ -23,7 +35,7 @@ def split(dataset, folds=2):
             # add random element
             ind = randrange(len(data))
             fold.append(data.pop(ind))
-            zfold.append(target.pop(ind))
+            zfold.append(t.pop(ind))
         splitset.append(fold)
         zset.append(zfold)
     return splitset, zset
@@ -32,18 +44,12 @@ def split(dataset, folds=2):
 seed(1)
 #print(np.array(kfold(train, 4)).shape)
 
-target = []
 
-for i in range(10):
-    ones = np.zeros((100,10))
-    ones[:, i] = 1
-    target.append(ones.tolist())
-target = np.reshape(np.ravel(target), (1000, 10))
 
 
 def ridge(phi, target, alpha):
     x = np.transpose(phi).dot(phi)
-    id = np.identity(len(phi[0]))
+    id = np.identity(phi[0].size)
     inv = np.linalg.inv(np.add(x, alpha*alpha*id))
 
     return inv.dot(np.transpose(phi)).dot(target)
@@ -60,18 +66,28 @@ def MSE(d, y):
 """
 array m..L would be an list of different model classes
 """
+
 def kfold(dataset, binaryset, models, alpha):
     #risklist = []
     res = []
     for i in range(len(dataset)):
         set = list(dataset)
 
-        validate = set[i]
-        corrects = binaryset[i]
-        train = set[np.arange(len(set)) != i]
-        target = binaryset[np.arange(len(set)) != i]
+        validate = np.array(set[i])
+        corrects = np.array(binaryset[i])
+        train = np.array(set)
+        train = np.delete(train, i, 0)
+        train = np.reshape(train, (750, 101))
+        print(train.shape)
+        target = np.array(binaryset)
+        target = np.delete(np.array(target), i, 0)
+        target = np.reshape(target, (750, 10))
+        print(target.shape)
         rlist = []
-        for m in range(1, len(models)+1):
+        for m in range(1):
+
+            print("Train:", train)
+            print("Target: ", target)
             dopt = ridge(train, target, alpha)
 
             trainvotes = np.array(train).dot(dopt)
@@ -80,16 +96,28 @@ def kfold(dataset, binaryset, models, alpha):
             testresult = MSE(testvotes, corrects)
 
 
-            rlist[0].append(trainresult)
-            rlist[1].append(testresult)
+            rlist.append(trainresult)
+            rlist.append(testresult)
 
             #r = (1/len(dataset)) * np.sum(loss(dopt, target))
             #rlist.append(r)
 
         #risk = np.mean(rlist)
+
         res.append(rlist)
-    print(res)
-    return np.argmin(res)
+    #print("Result is: ", res)
+    sum1 = 0
+    sum2= 0
+    for i in range(len(res)):
+        sum1 = sum1 + res[i][0]
+        sum2 = sum2 + res[i][1]
+
+    result = []
+    result.append(sum1/len(res))
+    result.append(sum2/len(res))
+    #return np.argmin(res) # for now return the result
+
+    return result
 
 
 def main():
@@ -102,10 +130,12 @@ def main():
     print(result)
 
 
-print(ridge(train, target, 0).shape)
+#print(ridge(train, target, 0))
+
+main()
+
 
 """
-
 x = np.linalg.pinv(train).dot(target)
 testvotes = np.array(test).dot(x)
 testresults = [max(p) for p in testvotes]
@@ -121,3 +151,4 @@ mismatches = np.sum(np.abs(np.sign(np.array(testresults)- np.array(np.ravel(corr
 
 error = 100*mismatches/1000
 """
+
