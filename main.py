2a92c2d56a2e6669560cbab71dc0c59907c78cf9
aksylumoed <adityadndkr@gmail.com>
2019-04-20 22:15:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import numpy as np
-import KmeansInitializations as km
-import PCA
-import matplotlib.pyplot as plt
-import Kfold as kf
+from tempfile import TemporaryFile
+import pca
+
 
 filename = "mfeat-pix.txt"
 digitImageVectors = np.loadtxt(filename)
 
+def createFeatureVectors():
+    method = 4
+    F = []
+    testVectors = []
+    for digit in range(0, 10):
+        left = int(digit)*200
+        right = 100 + 200*int(digit)
+        mfeat = digitImageVectors[left:right, :]
+        testVectors = np.append(testVectors, mfeat)
 
-
-# decide which initialization method to use
-print("Which initialization method do you want to use?\n")
-#method = input("Type 1 for Forgy method, 2 for Random Partition method, or 3 for kcluster ++ \n")
-method = 4
-F = []
-testVectors = []
-
-for digit in range(0, 10):
-    left = int(digit)*200
-    right = 2 + 200*int(digit)
-    mfeat = digitImageVectors[left:right, :5]
-    testVectors = np.append(testVectors, mfeat)
-    if int(method) in range(1, 4):
-        k = input("How many clusters do you want?\n")
-        km.kclusters(int(k), mfeat, digit, method)
-    else:
-        Fd = PCA.PCA(mfeat, digit)
+        Fd = pca.PCA(mfeat, digit)
         F.append(Fd)
 
-F = np.reshape(F, (10, 2, len(F[0][0])))
-testVectors = np.reshape(testVectors, (20, 5))#len(digitImageVectors[0])))
+    # F: Feature vectors
+    F = np.reshape(F, (10, 100, len(F[0][0])))
+    testVectors = np.reshape(testVectors, (1000, 240))
 
-binarySet = [np.identity(10)[i//2] for i in range(len(testVectors))]
-binarySet = np.reshape(binarySet, (len(testVectors), len(binarySet[0])))
+    return np.reshape(np.ravel(F), (1000, 101))
 
-kf.Kfold(testVectors, binarySet, 1, F, 2)
 
-plt.show()
+
+# training set
+
+
+#len(digitImageVectors[0])))
+
+# binary for training set
+#binarySet = [np.identity(10)[i//100] for i in range(len(testVectors))]
+#binarySet = np.reshape(binarySet, (len(testVectors), len(binarySet[0])))
+
+#print(np.reshape(np.ravel(F), (1000,101)).shape)
