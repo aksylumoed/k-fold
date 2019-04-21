2939582929d877c0c739d79bb4c325cd02ec6573
Oana Miron <oana9opt@yahoo.com>
2019-04-21 22:04:56 +0200

    Update features.py

--- features.py
+++ features.py
@@ -1,201 +1,50 @@
 import numpy as np
-from random import seed
-from random import randrange
-import features as feat
-import matplotlib.pyplot as plt
-import seaborn as sns
+from tempfile import TemporaryFile
+import help as pca
+from sklearn.decomposition import PCA
 
 
-"""
-RANDOMLY splits the dataset and binary set into n-folds
-"""
+filename = "mfeat-pix.txt"
+digitImageVectors = np.loadtxt(filename)
 
-#100 = 2 * 2 * 5 * 5
-# Possible folds {2,4,5,10,20,25,50,100}
-def split(dataset, train_y, folds=2):
-    t = train_y.tolist()
-    zset = []
-    splitset = []
-    data = list(dataset)
-    foldsize = int(len(data) / folds)/10
-    for i in range(folds):
-        fold = []
-        zfold = []
-        for j in range(0, 10):
-            k = 0
-            while k < foldsize:
-                ind = k + j*100
-                fold.append(data[ind])
-                zfold.append(t[ind])
-                k += 1
-        splitset.append(fold)
-        zset.append(zfold)
-    return splitset, zset
+def bias(transform):
+    fin = []
+    for i in range(len(transform)):
+        fin1 = np.append(transform[i], 1)
+        fin = np.append(fin, fin1)
+    fin = fin.reshape((len(transform),len(transform[0])+1))
+    return fin
 
+def createFeatureVectors(k):
+    method = 4
+    F = []
+    testVectors = []
+    for digit in range(0, 10):
+        left = int(digit)*200
+        right = 100 + 200*int(digit)
+        mfeat = digitImageVectors[left:right, :]
+        testVectors = np.append(testVectors, mfeat)
 
-seed(1)
+        pca = PCA(n_components=k)#(mfeat, digit, k)
+        Fd = bias(pca.fit_transform(mfeat))
+        print(Fd.shape)
+        F.append(Fd)
 
+    # F: Feature vectors
+    F = np.reshape(F, (10 * 100 , k+1))
+    testVectors = np.reshape(testVectors, (1000, 240))
 
-# print(np.array(kfold(train, 4)).shape)
-
-
-def ridge(phi, train_y, alpha):
-    x = np.transpose(phi).dot(phi)
-    id = np.identity(phi[0].size)
-    inv = np.linalg.inv(np.add(x, alpha * alpha * id))
-
-    return inv.dot(np.transpose(phi)).dot(train_y)
+    return np.reshape(np.ravel(F), (10 * 100, k+1))
 
 
-"""
-Calculates the Mean Squared Error
-"""
+
+# training set
 
 
-def MSE(d, y):
-    return np.sum((d - y) ** 2) / len(d)
+#len(digitImageVectors[0])))
 
+# binary for training set
+#binarySet = [np.identity(10)[i//100] for i in range(len(testVectors))]
+#binarySet = np.reshape(binarySet, (len(testVectors), len(binarySet[0])))
 
-def MISS(train_y, train_y_prediction):
-    miss = 0
-    for i in range(len(train_y)):
-        if np.argmax(train_y[i]) != np.argmax(train_y_prediction[i]):
-            miss += 1
-    return miss/len(train_y)
-
-
-def kfold(dataset, binaryset, models, alpha):
-    """
-
-    :param dataset: this dataset is the set of feature vectors, each with k features
-    :param binaryset: this is the set of binary train_y vectors z, where all values
-                      are 0 except 1 in index i, for class i
-    :param models: this is the model parameters we should modify
-                   (feature length, types of features)
-    :param alpha: alpha for the ridge regression
-    :return:
-    """
-    # risklist = []
-    res = []
-    #print(dataset)
-    for i in range(len(dataset)):
-        set = list(dataset)
-        size = len(set[0])
-        #print(size)
-        size *= len(set)
-
-        test_x = np.array(set[i])
-        print("Test")
-        #print(test_x)
-        test_y = np.array(binaryset[i])
-        train_x = np.array(set)
-        train_x = np.delete(train_x, i, 0)
-
-        print("train")
-        #print(train_x)
-        #print(train_x.shape)
-        train_x = np.reshape(train_x, (size-len(test_x), len(set[i][0])))
-
-        train_y = np.array(binaryset)
-        train_y = np.delete(np.array(train_y), i, 0)
-        train_y = np.reshape(train_y, (size-len(test_x), 10))
-
-
-        rlist = []  # use this to append the MSE values
-
-        Wopt = ridge(train_x, train_y, alpha)
-
-        train_y_pred = np.array(train_x).dot(Wopt)
-        test_y_pred = np.array(test_x).dot(Wopt)
-
-        train_MSE = MSE(train_y_pred, train_y)
-        test_MSE = MSE(test_y_pred, test_y)
-
-        train_MISS = MISS(train_y, train_y_pred)
-        test_MISS = MISS(test_y, test_y_pred)
-        #visualizePredictedAndExpected(test_y, testvotes)
-
-        rlist.append(train_MSE)
-        rlist.append(test_MSE)
-        rlist.append(train_MISS)
-        rlist.append(test_MISS)
-
-        # r = (1/len(dataset)) * np.sum(loss(dopt, train_y))
-        # rlist.append(r)
-
-        # risk = np.mean(rlist)
-
-        res.append(rlist)
-    # print("Result is: ", res)
-    sum1 = 0
-    sum2 = 0
-    sum3 = 0
-    sum4 = 0
-    for i in range(len(res)):
-        sum1 = sum1 + res[i][0]
-        sum2 = sum2 + res[i][1]
-        sum3 = sum3 + res[i][2]
-        sum4 = sum4 + res[i][3]
-
-    result = []
-    result.append(sum1 / len(res))
-    result.append(sum2 / len(res))
-    result.append(sum3 / len(res))
-    result.append(sum4 / len(res))
-    # return np.argmin(res) # for now return the result
-
-    return result
-
-
-# print(ridge(train, train_y, 0))
-
-#main()
-
-"""
-x = np.linalg.pinv(train).dot(train_y)
-testvotes = np.array(test).dot(x)
-testresults = [max(p) for p in testvotes]
-testresults = [x - 1 for x in testresults]
-
-test_y = []
-for i in range(10):
-    ones = np.zeros((1, 100))
-    ones[:] = i
-    test_y.append(ones.tolist())
-
-mismatches = np.sum(np.abs(np.sign(np.array(testresults)- np.array(np.ravel(test_y)))))
-
-error = 100*mismatches/1000
-"""
-
-
-def main():
-    filename = "mfeat-pix.txt"
-    data1 = np.loadtxt(filename)
-
-    w = [100]
-    for k in w:
-        data = feat.createFeatureVectors(k)
-        print(data.shape)
-        train = data.tolist()
-        test = data[1::2]
-
-        train_y = []
-
-        for i in range(10):
-            ones = np.zeros((100, 10))
-            ones[:, i] = 1
-            train_y.append(ones.tolist())
-        train_y = np.reshape(np.ravel(train_y), (1000, 10))
-
-        fold = [2, 4, 5, 10, 20, 25, 50, 100]
-        for foldSize in fold:
-            print(len(train))
-            dataset, binaryset = split(train, train_y, folds=foldSize )
-            print(len(dataset))
-            models = []
-            result = kfold(dataset, binaryset, models, 3)
-            print(result)
-        #plt.show()
-
-main()
+#print(np.reshape(np.ravel(F), (1000,101)).shape)
